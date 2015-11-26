f417057d86d1e5401208c07a86e7aaab61dab581
Karl Baum <karl.baum@gmail.com>
2015-11-26 12:59:52 -0500

    made test more robust

--- test/pivotal_bugsnacker/event_memory_test.rb
+++ test/pivotal_bugsnacker/event_memory_test.rb
@@ -8,9 +8,12 @@ module PivotalBugsnacker
 
     describe '#last_received' do
       it 'stores the last received as a Time' do
-        VCR.use_cassette('event_memory_last_received', record: :new_episodes) do
-          error = Bugsnag.errors.first
-          EventMemory.remember! error
+        VCR.use_cassette('event_memory_last_received', record: :none) do
+          errors = Bugsnag.errors(per_page: 5)
+          errors.each do |error|
+            EventMemory.remember! error
+          end
+          error = errors.first
           EventMemory.remember_last_received!
           EventMemory.last_received.must_equal error.last_received
         end
